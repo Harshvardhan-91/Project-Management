db7f53d87447c3491915713a7963821627993bc0
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-07-23 00:59:18 +0530

    Enhance Navbar and DashboardWrapper for improved state management and UI responsiveness

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="bg-red-500">hii</h1>
+        
       </main>
   );
 }
